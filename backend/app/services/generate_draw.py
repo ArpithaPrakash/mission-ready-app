74ab7a9dff64bcd9fbae0a88ade88998e61e302d
ArpithaPrakash <arpithap731@gmail.com>
2025-12-04 19:54:53 -0500

    Fix DB_PORT int conversion error by adding default value

--- backend/app/services/generate_draw.py
+++ backend/app/services/generate_draw.py
@@ -41,7 +41,7 @@ TRAINING_DIR = "MERGED_CONOPS_DRAWS"
 
 DB_CONN = {
     "host": os.getenv("DB_HOST"),
-    "port": int(os.getenv("DB_PORT")),
+    "port": int(os.getenv("DB_PORT", "5432")),
     "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
